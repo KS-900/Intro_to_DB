cd58333e3de78025a8ed6fde0476e8385e2d43cc
Karabo Satekge <kb2.satekge@gmail.com>
2025-02-09 19:01:39 +0200

    Task 5(2)

---
+++ task_5.sql
@@ -0,0 +1,5 @@
+-- Active: 1738658099745@@127.0.0.1@3306@alx_book_store
+-- Active: 1738658099745@@127.0.0.1@3306@abc
+USE alx_book_store;
+INSERT INTO customers(customer_id, customer_name, email, address)
+VALUES(1,'Cole Baidoo', 'cbaidoo@sandtech.com','123 Happiness Ave');
